621fb3a5bdacdb2462b26fd0cad8c0ef2a5ec70b
Arturo Amador <arturo.amadorb@hotmail.com>
2022-11-09 21:17:57 -0600

    Primera Entrega Arturo Amador

--- src/NavBar.js
+++ src/NavBar.js
@@ -8,28 +8,24 @@ function NavBar() {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">BoneHead Store</Navbar.Brand>
+                <Navbar.Brand href="#home"><img src='https://boneheadstore.000webhostapp.com/Multimedia/imagenes/logo6.png' width={150}></img></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#features">Más vendidos</Nav.Link>
-                        <Nav.Link href="#pricing">Tiendas</Nav.Link>
+                        <Nav.Link href="#MasVendidos">Más vendidos</Nav.Link>
+                        <Nav.Link href="#Tiendas">Tiendas</Nav.Link>
                         <NavDropdown title="Instrumentos" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Guitarras</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Baterías
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Bajos</NavDropdown.Item>
+                            <NavDropdown.Item href="#Guitarras">Guitarras</NavDropdown.Item>
+                            <NavDropdown.Item href="#Baterias">Baterías</NavDropdown.Item>
+                            <NavDropdown.Item href="#Bajos">Bajos</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#deets">Iniciar sesión</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Crear Cuenta
-                        </Nav.Link>
+                        <Nav.Link href="#IniciarSesion">Iniciar sesión</Nav.Link>
+                        <Nav.Link eventKey={2} href="#CrearCuenta">Crear Cuenta</Nav.Link>
                     </Nav>
                     <Nav>
-                        <Nav.Link><img src=''></img></Nav.Link>
+                        <Nav.Link href='#Cart' ><img src='https://icons.veryicon.com/png/o/miscellaneous/template-3/shopping-cart-94.png' width={45} ></img></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
